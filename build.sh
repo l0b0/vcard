1210daebdb84050d088c9f246f02ce796bc49152
Victor Engmark <victor.engmark@gmail.com>
2010-03-17 14:18:12 +0000

    Proper cleanup after uploading
    git-svn-id: https://vcard-module.svn.sourceforge.net/svnroot/vcard-module@24 1095d6e4-126c-4afa-87e3-fb20f22b6714

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-python setup.py test && python setup.py $1 bdist_egg bdist_rpm bdist_wininst sdist upload clean
+python setup.py test && python setup.py $1 bdist_egg bdist_rpm bdist_wininst sdist upload clean && rm -r build dist vCard_module.egg-info *.pyc
