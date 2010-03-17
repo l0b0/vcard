9c1301e7d9d05957b85243a89bc43d9fe1296913
Victor Engmark <victor.engmark@gmail.com>
2010-03-17 15:15:37 +0000

    A couple more test cases.
    git-svn-id: https://vcard-module.svn.sourceforge.net/svnroot/vcard-module@25 1095d6e4-126c-4afa-87e3-fb20f22b6714

--- vcard_test.py
+++ vcard_test.py
@@ -5,8 +5,7 @@
 Default syntax:
 
 ./vcard_test.py
-
-Run all unit tests
+    Run all unit tests
 """
 import unittest
 import warnings
@@ -30,18 +29,21 @@ VCARDS_INVALID_PARAM = {
 VCARDS_INVALID_PARAM_VALUE = {
     }
 VCARDS_INVALID_VALUE = {
-    'Wikipedia article': u"""BEGIN:VCARD\r\nVERSION:3.0\r\nN:Gump;Forrest\r\nFN:Forrest Gump\r\nORG:Bubba Gump Shrimp Co.\r\nTITLE:Shrimp Man\r\nTEL;TYPE=WORK,VOICE:(111) 555-1212\r\nTEL;TYPE=HOME,VOICE:(404) 555-1212\r\nADR;TYPE=WORK:;;100 Waters Edge;Baytown;LA;30314;United States of America\r\nLABEL;TYPE=WORK:100 Waters Edge\\nBaytown, LA 30314\\nUnited States of America\r\nADR;TYPE=HOME:;;42 Plantation St.;Baytown;LA;30314;United States of America\r\nLABEL;TYPE=HOME:42 Plantation St.\\nBaytown, LA 30314\\nUnited States of America\r\nEMAIL;TYPE=PREF,INTERNET:forrestgump@example.com\r\nREV:20080424T195243Z\r\nEND:VCARD\r\n""",
+    'http://en.wikipedia.org/wiki/VCard': u"""BEGIN:VCARD\r\nVERSION:3.0\r\nN:Gump;Forrest\r\nFN:Forrest Gump\r\nORG:Bubba Gump Shrimp Co.\r\nTITLE:Shrimp Man\r\nTEL;TYPE=WORK,VOICE:(111) 555-1212\r\nTEL;TYPE=HOME,VOICE:(404) 555-1212\r\nADR;TYPE=WORK:;;100 Waters Edge;Baytown;LA;30314;United States of America\r\nLABEL;TYPE=WORK:100 Waters Edge\\nBaytown, LA 30314\\nUnited States of America\r\nADR;TYPE=HOME:;;42 Plantation St.;Baytown;LA;30314;United States of America\r\nLABEL;TYPE=HOME:42 Plantation St.\\nBaytown, LA 30314\\nUnited States of America\r\nEMAIL;TYPE=PREF,INTERNET:forrestgump@example.com\r\nREV:20080424T195243Z\r\nEND:VCARD\r\n""",
+    'http://microformats.org/tests/hcard/01-tantek-basic.vcf': u"""BEGIN:VCARD\r\nFN;CHARSET=UTF-8:Tantek Çelik\r\nN;CHARSET=UTF-8:Çelik;Tantek;;;\r\nNAME:01-tantek-basic\r\nORG;CHARSET=UTF-8:Technorati\r\nPRODID:$PRODID$\r\nSOURCE:http://microformats.org/test/hcard/01-tantek-basic.html\r\nURL:http://tantek.com/\r\nVERSION:3.0\r\nEND:VCARD\r\n""",
     }
 
 # Valid vCards
 VCARDS_VALID = {
     'minimal': 'BEGIN:VCARD\r\nVERSION:3.0\r\nN:Doe;John;;Mr;\r\nFN:John Doe\r\nEND:VCARD\r\n',
     'scrambled_case': 'bEGIn:VCaRD\r\nVErsiON:3.0\r\nn:Doe;John;;Mr;\r\nfN:John Doe\r\nEnD:vcArD\r\n',
-    'aspaass.no': u"""BEGIN:VCARD\r\nVERSION:3.0\r\nPROFILE:VCARD\r\nCLASS:PUBLIC\r\nSOURCE:http\\://aspaass.no/Aspaas Sykler.vcf\r\nN:Aspaas Sykler;;;;\r\nFN:Aspaas Sykler\r\nORG:Aspaas Sykler\r\nURL;TYPE=WORK:http\\://aspaass.no/\r\nEMAIL;TYPE=INTERNET;TYPE=PREF;TYPE=WORK:aspaass@online.no\r\nTEL;TYPE=VOICE;TYPE=PREF;TYPE=MSG;TYPE=WORK:+4733187525\r\nTEL;TYPE=FAX;TYPE=WORK:+4733183703\r\nADR:;;Nansetgata 74;Larvik;Vestfold;3269;Norway\r\nLABEL:Nansetgata 74\\n3269 Larvik\\nNorway\r\nPHOTO;VALUE=uri:http\\://aspaass.no/include/image/butikk.jpg\r\nNOTE:Åpningstider mandag til fredag 9-18; lørdag 9-16\r\nTZ:+01\\:00\r\nGEO:59.06116,10.03712\r\nLOGO;VALUE=uri:http\\://aspaass.no/include/image/logo.png\r\nCATEGORIES:business,Norwegian\r\nEND:VCARD\r\n"""}
+    'http://aspaass.no/kontakt/Aspaas%20Sykler.vcf': u"""BEGIN:VCARD\r\nVERSION:3.0\r\nPROFILE:VCARD\r\nCLASS:PUBLIC\r\nSOURCE:http\\://aspaass.no/Aspaas Sykler.vcf\r\nN:Aspaas Sykler;;;;\r\nFN:Aspaas Sykler\r\nORG:Aspaas Sykler\r\nURL;TYPE=WORK:http\\://aspaass.no/\r\nEMAIL;TYPE=INTERNET;TYPE=PREF;TYPE=WORK:aspaass@online.no\r\nTEL;TYPE=VOICE;TYPE=PREF;TYPE=MSG;TYPE=WORK:+4733187525\r\nTEL;TYPE=FAX;TYPE=WORK:+4733183703\r\nADR:;;Nansetgata 74;Larvik;Vestfold;3269;Norway\r\nLABEL:Nansetgata 74\\n3269 Larvik\\nNorway\r\nPHOTO;VALUE=uri:http\\://aspaass.no/include/image/butikk.jpg\r\nNOTE:Åpningstider mandag til fredag 9-18; lørdag 9-16\r\nTZ:+01\\:00\r\nGEO:59.06116,10.03712\r\nLOGO;VALUE=uri:http\\://aspaass.no/include/image/logo.png\r\nCATEGORIES:business,Norwegian\r\nEND:VCARD\r\n""",
+    'http://www.troywolf.com/articles/php/class_vcard/vcard_example.php': """BEGIN:VCARD\r\nVERSION:3.0\r\nCLASS:PUBLIC\r\nPRODID:-//class_vcard from TroyWolf.com//NONSGML Version 1//EN\r\nREV:2010-03-17 09:36:59\r\nFN:Troy Wolf\r\nN:Wolf;Troy;;;\r\nNICKNAME:TJ\r\nTITLE:Web Developer\r\nORG:TroyWolf.com\r\nADR;TYPE=work:;;7027 N. Hickory;Kansas City;MO;64118;\r\nADR;TYPE=home:;;7027 N. Hickory;Kansas City;MO;64118;\r\nEMAIL;TYPE=internet,pref:troy@troywolf.com\r\nTEL;TYPE=cell,voice:(816) 739-9653\r\nURL;TYPE=work:http://www.troywolf.com\r\nBDAY:1971-08-13\r\nTZ:-06:00\r\nEND:VCARD\r\n""",
+    }
 
 VCARDS_REFERENCE = {
-    'rfc 2426 1': u"""BEGIN:vCard\r\nVERSION:3.0\r\nFN:Frank Dawson\r\nORG:Lotus Development Corporation\r\nADR;TYPE=WORK,POSTAL,PARCEL:;;6544 Battleford Drive\r\n ;Raleigh;NC;27613-3502;U.S.A.\r\nTEL;TYPE=VOICE,MSG,WORK:+1-919-676-9515\r\nTEL;TYPE=FAX,WORK:+1-919-676-9564\r\nEMAIL;TYPE=INTERNET,PREF:Frank_Dawson@Lotus.com\r\nEMAIL;TYPE=INTERNET:fdawson@earthlink.net\r\nURL:http://home.earthlink.net/~fdawson\r\nEND:vCard\r\n""",
-    'rfc 2426 2': u"""BEGIN:vCard\r\nVERSION:3.0\r\nFN:Tim Howes\r\nORG:Netscape Communications Corp.\r\nADR;TYPE=WORK:;;501 E. Middlefield Rd.;Mountain View;\r\n CA; 94043;U.S.A.\r\nTEL;TYPE=VOICE,MSG,WORK:+1-415-937-3419\r\nTEL;TYPE=FAX,WORK:+1-415-528-4164\r\nEMAIL;TYPE=INTERNET:howes@netscape.com\r\nEND:vCard\r\n"""}
+    'http://tools.ietf.org/html/rfc2426 1': u"""BEGIN:vCard\r\nVERSION:3.0\r\nFN:Frank Dawson\r\nORG:Lotus Development Corporation\r\nADR;TYPE=WORK,POSTAL,PARCEL:;;6544 Battleford Drive\r\n ;Raleigh;NC;27613-3502;U.S.A.\r\nTEL;TYPE=VOICE,MSG,WORK:+1-919-676-9515\r\nTEL;TYPE=FAX,WORK:+1-919-676-9564\r\nEMAIL;TYPE=INTERNET,PREF:Frank_Dawson@Lotus.com\r\nEMAIL;TYPE=INTERNET:fdawson@earthlink.net\r\nURL:http://home.earthlink.net/~fdawson\r\nEND:vCard\r\n""",
+    'http://tools.ietf.org/html/rfc2426 2': u"""BEGIN:vCard\r\nVERSION:3.0\r\nFN:Tim Howes\r\nORG:Netscape Communications Corp.\r\nADR;TYPE=WORK:;;501 E. Middlefield Rd.;Mountain View;\r\n CA; 94043;U.S.A.\r\nTEL;TYPE=VOICE,MSG,WORK:+1-415-937-3419\r\nTEL;TYPE=FAX,WORK:+1-415-528-4164\r\nEMAIL;TYPE=INTERNET:howes@netscape.com\r\nEND:vCard\r\n"""}
 
 # pylint: enable-msg=C0301
 
@@ -97,7 +99,6 @@ class TestVCards(unittest.TestCase):
 
 def main():
     """Command line options checkpoint"""
-    #print(VCARD_ASPAASS_NO)
     unittest.main()
 
 if __name__ == '__main__':
