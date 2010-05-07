404123309946a3d61e7d4d4e20709f1faf9f5263
Victor Engmark <victor.engmark@gmail.com>
2010-05-07 14:05:08 +0000

    Go to source directory before building.
    git-svn-id: https://vcard-module.svn.sourceforge.net/svnroot/vcard-module@46 1095d6e4-126c-4afa-87e3-fb20f22b6714

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Requires rpm package
+cd $(dirname $0) && \
 python setup.py test && \
 python setup.py $1 bdist_egg bdist_rpm bdist_wininst sdist upload clean && \
-rm -r build dist vCard_module.egg-info *.pyc
+rm -fr *.pyc build dist temp vCard_module.egg-info
