3eb3f83209c29035f1d49d1ebe8ae37f8067e931
Victor Engmark <victor.engmark@gmail.com>
2012-05-18 12:09:24 +0200

    No longer using a build script

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Requires rpm package
-cd $(dirname $0) && \
-python setup.py test && \
-python setup.py $1 bdist_egg bdist_rpm bdist_wininst sdist upload clean && \
-rm -fr *.pyc build dist temp vCard_module.egg-info
