9f0087934f24979a6133b13c98e90f9691aa0c5b
Victor Engmark <victor.engmark@gmail.com>
2012-06-11 03:09:33 +0200

    Split a multiple vCards file into individual files

---
+++ split.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+#
+# NAME
+#        split.sh - Split a multiple vCards file into individual files
+#
+# SYNOPSIS
+#        split.sh FILE...
+#
+# DESCRIPTION
+#        Can be used to process vCards individually. Restore the original file
+#        with `cat xx* > original.vcf`.
+#
+#        See `man csplit` for options.
+#
+# BUGS
+#        https://github.com/l0b0/split/issues
+#
+# COPYRIGHT
+#        Copyright (C) 2012 Victor Engmark
+#
+#        This program is free software: you can redistribute it and/or modify
+#        it under the terms of the GNU General Public License as published by
+#        the Free Software Foundation, either version 3 of the License, or
+#        (at your option) any later version.
+#
+#        This program is distributed in the hope that it will be useful,
+#        but WITHOUT ANY WARRANTY; without even the implied warranty of
+#        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#        GNU General Public License for more details.
+#
+#        You should have received a copy of the GNU General Public License
+#        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
+set -o errexit -o noclobber -o nounset -o pipefail
+
+csplit --elide-empty-files "$@" $'/^BEGIN:VCARD\r$/' {*}
